769246d5aae15577dfadc4205b2141eea72d8880
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-01-09 10:54:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const product = {
+    title: "Ball Pen", 
+    rating: 4, 
+    offer: 5, 
+    price: 270, 
+}; 
+
+console.log(product);
+
+const student = {
+    fullName : "Abdul Majeed", 
+    rollNumber: 96, 
+    class: "IT", 
+    isPass: false, 
+};
+
+console.log(student);
